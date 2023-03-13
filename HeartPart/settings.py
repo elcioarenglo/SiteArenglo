268382af4303ef270e76fa2ac2d06f3e4affc574
elcioarenglo <elcio@arenglobrasil.com.br>
2023-03-13 18:32:31 -0300

    'update1'

--- HeartPart/settings.py
+++ HeartPart/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x_m-r#b=^d6lpepr*n)9#fy4#9(a*03n+=+(088_&=irtf77oh" 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
@@ -125,7 +125,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR/'MainPageArenglo'/'static',
     ]
-
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
